9ef5686673a7248bbf4bf51363d193ec9b37072b
is3nsu <p.nakraszewicz@gmail.com>
2026-02-03 20:02:58 +0100

    fixes

--- base64-decoder/b64dec.py
+++ base64-decoder/b64dec.py
@@ -1,27 +1,36 @@
 import sys
 import base64
 
-try: 
-    source_file = sys.argv[1]
-    target_file = sys.argv[2]
+source_file = sys.argv[1]
+target_file = sys.argv[2] 
+mode = None
+
+if len(sys.argv) == 4:
     mode = sys.argv[3]
+elif len(sys.argv) != 3:
+    print("Use: b64dec <input> <output> [--bin|-b|--txt|-t]")
+    sys.exit(1)
 
-    with open(source_file, "r") as source:
-        content = source.read().strip()
-    decoded = base64.b64decode(content + '===')
+with open(source_file, "r") as source:
+    content = source.read().strip()
+decoded = base64.b64decode(content + '===')
 
-    
-    if mode in ('--bin' or '-b'):
-        with open(target_file, 'wb') as target:
+if mode in ('--bin', '-b'):
+    with open(target_file, 'wb') as target:
+        target.write(decoded)
+elif mode in ('--txt', '-t'): 
+    try: 
+        with open(target_file, "w") as target:
+            target.write(decoded.decode('utf-8'))
+    except UnicodeDecodeError: # if decoded is binary, force binary
+        with open(target_file, "wb") as target:
             target.write(decoded)
-    elif mode in ('--txt' or '-t'): 
-        try: 
-            with open(target_file, "w") as target:
-                target.write(decoded.decode('utf-8'))
-        except UnicodeDecodeError: # if decoded is binary, force binary
-            with open(target_file, "wb") as target:
-                target.write(decoded)
+elif mode is None:
+    try: 
+        with open(target_file, "w") as target:
+            target.write(decoded.decode('utf-8'))
+    except UnicodeDecodeError: # if decoded is binary, force binary
+        with open(target_file, "wb") as target:
+            target.write(decoded)
 
-except IndexError:
-    print('Use: python3 b64dec.py <filename>.txt <filename2>.txt --output_type')
 
